79b9074a13e6f669aea61f7e7b4d35b1215de2f2
DalCed <cbi2173010030@izt.uam.mx>
2021-08-17 19:08:08 -0500

    Se agregaron el archivo.C y JSON

---
+++ Hola_mundo.c
@@ -0,0 +1,26 @@
+/*Comentarios son para el yo del futuro, llevan:
+    * Nombre del programa
+    * Autor
+    * Modificado por
+    * ¿Qué hace el programa?
+    * ¿Se requiere un circuito?
+    * Si la respuesta anterior es si, colocar una imagen del circuito en el repositorio
+    * Terminales que se van a usar y el modo
+        ejemplo: GPIO5: OUT;
+*/
+
+/*********Bibliotecas******************/
+#include<stdio.h>
+
+/*Declaración de variables*/
+    //#define char frase="Hola Mundo";
+
+/*Declarar las funciones que se mandan a llamar*/
+
+
+/*Estructura principal del programa Main*/
+int main(){
+    printf("Hola mundo");
+
+    return 0;
+}
